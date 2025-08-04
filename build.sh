adb149ca39c50182ba2df384e577ff78f22f4875
gokul <gopalakrishnan93843@gmail.com>
2025-08-04 15:40:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-docker build -t devops-react-app .
+set -e
 
+echo "Installing dependencies..."
+npm install
+
+echo "Building React app..."
+npm run build
+
+echo "Building Docker image: $1"
+docker build -t $1 .
