ea7b38a0f06d7731dcb947564d07452eac319ec2
gokul <gopalakrishnan93843@gmail.com>
2025-08-04 13:12:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-BRANCH=$1
-if [ "$BRANCH" == "dev" ]; then
-  IMAGE="gokul603/devops-react-dev"
-elif [ "$BRANCH" == "prod" ]; then
-  IMAGE="gokul603/devops-react-prod"
-else
-  echo "❌ Unknown branch"
-  exit 1
-fi
-
-echo "🚀 Deploying image: $IMAGE"
-docker pull $IMAGE
-docker stop react-app || true
-docker rm react-app || true
-docker run -d --name react-app -p 80:80 $IMAGE
-
-
-
-
-
+docker run -d -p 80:80 devops-react-app
