10e667597ac764059ab4dcd569bc0a0da0770cd9
gokul <gopalakrishnan93843@gmail.com>
2025-08-04 13:11:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-BRANCH=$1
-if [ "$BRANCH" == "dev" ]; then
-  IMAGE="gokul603/devops-react-dev"
-elif [ "$BRANCH" == "prod" ]; then
-  IMAGE="gokul603/devops-react-prod"
-else
-  echo "❌ Unknown branch"
-  exit 1
-fi
-
-echo "🛠️ Building and pushing image: $IMAGE"
-docker build -t $IMAGE .
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker push $IMAGE
+docker build -t devops-react-app .
 
