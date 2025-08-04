bd152cef896aff52884a354aefa2e926fdccc17c
gokul <gopalakrishnan93843@gmail.com>
2025-08-04 16:18:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo "👉 Installing dependencies..."
-npm install --verbose
-
-echo "✅ Dependencies installed."
-
-echo "👉 Building React app..."
-npm run build
-
-echo "✅ React build complete."
 
 echo "👉 Building Docker image..."
 docker build -t $1 .
