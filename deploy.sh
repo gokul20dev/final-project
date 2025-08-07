8ec3fcefbb261e0d6859a614350b238ed7f98222
gokul <gopalakrishnan93843@gmail.com>
2025-08-07 15:41:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "👉  Pushing image: $IMAGE_NAME to Docker Hub..."
 docker pull $IMAGE_NAME
 
 echo "👉  Deploying using docker-compose..."
-docker compose down || true
-docker compose up -d --build
+docker-compose down || true
+docker-compose up -d --build
 
 echo "✅  Deployment completed from image: $IMAGE_NAME"
